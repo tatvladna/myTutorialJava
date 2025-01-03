f79d2ea341c076a70c9b58da0f77cf5a9c960d9c
tatvladna <tpustovalova@mail.ru>
2025-01-04 00:49:15 +0300

    my changes added

--- src/shop/Category.java
+++ src/shop/Category.java
@@ -1,4 +1,10 @@
 package shop;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Category {
+    private String name;
+    private List<String> categoryProducts = new ArrayList<>();
+
+    public
 }
--- src/shop/Product.java
+++ src/shop/Product.java
@@ -1,4 +1,38 @@
 package shop;
 
 public class Product {
+    private String name;
+    private double price;
+    private double rating;
+
+    public Product(String name, double price, double rating) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public double getRating() {
+        return rating;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+    @Override
+    public String toString() {
+        return "Product: " +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", rating=" + rating;
+    }
 }
